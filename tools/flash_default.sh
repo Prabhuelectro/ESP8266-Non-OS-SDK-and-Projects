d53aa2a26a39b6e8d8e9a506fe1c1314a76a2381
Prabhu <prabhuelectro99@gmail.com>
2020-09-02 08:03:29 +0530

    Flash scripts Bin folder

--- tools/flash_default.sh
+++ tools/flash_default.sh
@@ -8,5 +8,8 @@ F_MODE=dio
 
 F_FREQ=40m
 
+BIN_FOLDER=../bin
 
-esptool.py --chip esp8266 --port $PORT --baud $BAUD --before default_reset --after hard_reset write_flash --flash_mode $F_MODE --flash_freq $F_FREQ --flash_size $F_SIZE 0xFB000 /home/prabhu/workspace/ESP8266/ESP8266_NONOS/bin/blank.bin 0xFC000 /home/prabhu/workspace/ESP8266/ESP8266_NONOS/bin/esp_init_data_default_v08.bin 0xFE000 /home/prabhu/workspace/ESP8266/ESP8266_NONOS/bin/blank.bin
+chmod +rwx $PORT
+
+esptool.py --chip esp8266 --port $PORT --baud $BAUD --before default_reset --after hard_reset write_flash --flash_mode $F_MODE --flash_freq $F_FREQ --flash_size $F_SIZE 0xFB000 ${BIN_FOLDER}/blank.bin 0xFC000 ${BIN_FOLDER}/esp_init_data_default_v08.bin 0xFE000 ${BIN_FOLDER}/blank.bin
--- tools/flash_firmware.sh
+++ tools/flash_firmware.sh
@@ -8,6 +8,8 @@ F_MODE=dio
 
 F_FREQ=40m
 
-chmod +rwx /dev/ttyUSB0
+BIN_FOLDER=../bin
 
-esptool.py --chip esp8266 --port $PORT --baud $BAUD --before default_reset --after hard_reset write_flash --flash_mode $F_MODE --flash_freq $F_FREQ --flash_size $F_SIZE 0x0000 /home/prabhu/workspace/ESP8266/ESP8266_NONOS/bin/eagle.flash.bin 0x10000 /home/prabhu/workspace/ESP8266/ESP8266_NONOS/bin/eagle.irom0text.bin
+chmod +rwx $PORT
+
+esptool.py --chip esp8266 --port $PORT --baud $BAUD --before default_reset --after hard_reset write_flash --flash_mode $F_MODE --flash_freq $F_FREQ --flash_size $F_SIZE 0x0000 ${BIN_FOLDER}/eagle.flash.bin 0x10000 ${BIN_FOLDER}/eagle.irom0text.bin
